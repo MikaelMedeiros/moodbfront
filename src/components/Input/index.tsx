d95281ba7b2d41a427e3e22d5523426728c6ff14
Mikael Medeiros <mikaeltruth@gmail.com>
2022-04-26 00:58:49 -0300

    login and inputs

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,10 +1,12 @@
-// @flow 
 import * as React from 'react';
+import styles from '././Input.module.css';
+
 type InputProps = {
     placeholder: string,
+    type: string
 };
 export const Input: React.FunctionComponent<InputProps> = (props) => {
     return (
-        <input className='border-b-2 w-full' placeholder={props.placeholder}></input>
+        <input type={props.type} className={styles.root} placeholder={props.placeholder}></input>
     );
 };
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,19 +7,24 @@ const TailwindcssPage: NextPage  = () => {
     return (
         <div className="flex flex-col text-center container mx-auto px-16">
             <div>
-                <h1 className='text-4xl mt-20 text-gray-600 align-baseline'>
+                <h1 className='text-4xl mt-20 text-gray-500 align-baseline'>
                     MoodB
                 </h1>
             </div>
-            
-            <h2 className='text-left text-xl mt-20'>Login</h2>    
-            <div className="flex flex-col space-y-5 items-center">
-                <Input placeholder='E-mail'></Input>
-                <Input placeholder='Password'></Input>
+            <div>
+                <h2 className='text-left text-xl font-bold text-gray-400 mt-20'>Login</h2>    
+            </div>
+            <div className="flex flex-col space-y-5 items-center mt-5">
+                <Input type='text' placeholder='E-mail'></Input>
+                <Input type='password' placeholder='Senha'></Input>
             </div>
 
-            <div className='mt-2'>
+            <div className='mt-10'>
                 <Button>Entrar</Button>
+            </div>
+
+            <div className='text-xs italic text-gray-500 absolute inset-x-0 bottom-4'>
+                &copy;2022 Powered by YSNP. All rights reserved.
             </div>
         </div>
     );
